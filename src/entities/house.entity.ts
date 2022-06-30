3c68351ba2425559632b0d58cdbf4284edd78262
NickSettler <moiseevnikita14@gmail.com>
2022-06-30 18:49:05 +0200

    (refactor) introduces separate variables for HouseEntity keys and foreign keys

--- src/entities/house.entity.ts
+++ src/entities/house.entity.ts
@@ -12,9 +12,24 @@ import { Point } from 'geojson';
 import UserEntity from './user.entity';
 import { JoinColumn } from 'typeorm';
 
-enum HOUSE_STATUSES {
+export enum HOUSE_STATUSES {
   ACTIVE = 'ACTIVE',
   DELETED = 'DELETED',
+}
+
+export enum HOUSE_FOREIGN_KEYS {
+  OWNER = 'fk_houses_owner',
+}
+
+export enum HOUSE_ENTITY_KEYS {
+  ID = 'id',
+  NAME = 'name',
+  STATUS = 'status',
+  LOCATION = 'location',
+  OWNER = 'owner',
+  CREATED_AT = 'created_at',
+  UPDATED_AT = 'updated_at',
+  DELETED_AT = 'deleted_at',
 }
 
 @Entity({
@@ -22,13 +37,13 @@ enum HOUSE_STATUSES {
 })
 export default class HouseEntity {
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  [HOUSE_ENTITY_KEYS.ID]: string;
 
   @Column({
     type: 'varchar',
     nullable: false,
   })
-  name: string;
+  [HOUSE_ENTITY_KEYS.NAME]: string;
 
   @Column({
     type: 'enum',
@@ -36,26 +51,24 @@ export default class HouseEntity {
     enum: Object.values(HOUSE_STATUSES),
     default: HOUSE_STATUSES.ACTIVE,
   })
-  status: HOUSE_STATUSES;
+  [HOUSE_ENTITY_KEYS.STATUS]: HOUSE_STATUSES;
 
   @Column({
     type: 'geography',
     nullable: true,
   })
-  location: Point | (() => string);
+  [HOUSE_ENTITY_KEYS.LOCATION]: Point | (() => string);
 
   @ManyToOne(() => UserEntity)
   @JoinColumn({ name: 'owner' })
-  owner: UserEntity;
+  [HOUSE_ENTITY_KEYS.OWNER]: UserEntity;
 
   @CreateDateColumn()
-  created_at: Date;
+  [HOUSE_ENTITY_KEYS.CREATED_AT]: Date;
 
   @UpdateDateColumn()
-  updated_at: Date;
+  [HOUSE_ENTITY_KEYS.UPDATED_AT]: Date;
 
   @DeleteDateColumn()
-  deleted_at: Date;
+  [HOUSE_ENTITY_KEYS.DELETED_AT]: Date;
 }
-
-export { HOUSE_STATUSES };
