3b9658fc1500f738d3d41d6999bfbf703312bfcf
NickSettler <moiseevnikita14@gmail.com>
2022-07-14 01:43:34 +0200

    (refactor) moves adding new columns to separate migration file

---
+++ src/db/migrations/1657755500678-UpdateUserTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { DB_TABLES } from '../constants';
+import { USER_ENTITY_KEYS } from '../../entities/user.entity';
+
+export class UpdateUserTable1657755500678 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumns(DB_TABLES.USERS, [
+      new TableColumn({
+        name: USER_ENTITY_KEYS.USERNAME,
+        type: 'varchar',
+        isNullable: false,
+        isUnique: true,
+      }),
+      new TableColumn({
+        name: USER_ENTITY_KEYS.FIRST_NAME,
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: USER_ENTITY_KEYS.LAST_NAME,
+        type: 'varchar',
+        isNullable: true,
+      }),
+    ]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumns(DB_TABLES.USERS, [
+      USER_ENTITY_KEYS.USERNAME,
+      USER_ENTITY_KEYS.FIRST_NAME,
+      USER_ENTITY_KEYS.LAST_NAME,
+    ]);
+  }
+}
