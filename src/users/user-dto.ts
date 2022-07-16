d9594b0f1125d002cd12bdfaeb4b94c10467b603
NickSettler <moiseevnikita14@gmail.com>
2022-07-16 15:38:38 +0200

    (feat) adds user module docs

--- src/users/user-dto.ts
+++ src/users/user-dto.ts
@@ -6,46 +6,109 @@ import {
   MinLength,
 } from 'class-validator';
 import { USER_ENTITY_KEYS } from '../entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.USERNAME,
+    description: 'Username',
+    type: 'string',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   [USER_ENTITY_KEYS.USERNAME]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.EMAIL,
+    description: 'Email',
+    type: 'string',
+    required: true,
+  })
   @IsNotEmpty()
   @IsEmail()
   [USER_ENTITY_KEYS.EMAIL]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.PASSWORD,
+    description: 'Password',
+    type: 'string',
+    format: 'password',
+    required: true,
+  })
   @IsNotEmpty()
   @MinLength(6)
   [USER_ENTITY_KEYS.PASSWORD]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.FIRST_NAME,
+    description: 'First name',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   [USER_ENTITY_KEYS.FIRST_NAME]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.LAST_NAME,
+    description: 'Last name',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   [USER_ENTITY_KEYS.LAST_NAME]: string;
 }
 
 export class UpdateUserDto {
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.USERNAME,
+    description: 'Username',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   [USER_ENTITY_KEYS.USERNAME]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.EMAIL,
+    description: 'Email',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsEmail()
   [USER_ENTITY_KEYS.EMAIL]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.PASSWORD,
+    description: 'Password',
+    type: 'string',
+    format: 'password',
+    required: false,
+  })
   @IsOptional()
   @MinLength(6)
   [USER_ENTITY_KEYS.PASSWORD]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.FIRST_NAME,
+    description: 'First name',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   [USER_ENTITY_KEYS.FIRST_NAME]: string;
 
+  @ApiProperty({
+    name: USER_ENTITY_KEYS.LAST_NAME,
+    description: 'Last name',
+    type: 'string',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   [USER_ENTITY_KEYS.LAST_NAME]: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -23,7 +23,23 @@ import SentryInterceptor from '../common/interceptors/sentry.interceptor';
 import { Request } from 'express';
 import UserEntity, { USER_ENTITY_KEYS } from '../entities/user.entity';
 import { Like } from 'typeorm';
+import {
+  ApiBody,
+  ApiConflictResponse,
+  ApiCookieAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('users')
+@ApiCookieAuth()
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
 @UseInterceptors(SentryInterceptor, ClassSerializerInterceptor)
 @Controller('users')
 export class UsersController {
@@ -32,6 +48,17 @@ export class UsersController {
   @Get()
   @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    description: 'Users array',
+    type: UserEntity,
+    isArray: true,
+  })
+  @ApiQuery({
+    type: 'string',
+    name: 'q',
+    description: 'Search query',
+    required: false,
+  })
   public async getUsers(
     @Query('q') query?: string,
   ): Promise<Array<UserEntity>> {
@@ -43,6 +70,13 @@ export class UsersController {
   @Get('/me')
   @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    description: 'User',
+    type: UserEntity,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
   public async getAuthenticatedUser(
     @Req() request: Request,
   ): Promise<UserEntity> {
@@ -54,6 +88,15 @@ export class UsersController {
   @Post()
   @UsePipes(ValidationPipe)
   @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'User',
+    type: UserEntity,
+  })
+  @ApiConflictResponse({ description: 'User already exists' })
+  @ApiBody({
+    description: 'Create user DTO',
+    type: CreateUserDto,
+  })
   public async createUser(
     @Body() createUserDto: CreateUserDto,
   ): Promise<UserEntity> {
@@ -63,6 +106,24 @@ export class UsersController {
   @Put('/:id')
   @UsePipes(ValidationPipe)
   @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'User updated',
+    type: UserEntity,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
+  @ApiConflictResponse({ description: 'User already exists' })
+  @ApiParam({
+    name: 'id',
+    description: 'User ID',
+    type: 'string',
+    required: true,
+  })
+  @ApiBody({
+    description: 'Update user DTO',
+    type: UpdateUserDto,
+  })
   public async updateUser(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -73,6 +134,18 @@ export class UsersController {
   @Delete('/:id')
   @UsePipes(ValidationPipe)
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiNoContentResponse({
+    description: 'User deleted',
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'User ID',
+    type: 'string',
+    required: true,
+  })
   public async deleteAllUsers(@Param('id') id: string): Promise<void> {
     return this.usersService.delete(id);
   }
