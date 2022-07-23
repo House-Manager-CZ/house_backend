977889484d1df0a4768c5055d7194f03712fdf1f
Nick Settler <moiseevnikita14@gmail.com>
2022-07-24 01:56:19 +0200

    (feat) adds members addition on house create and update (#50)

--- src/houses/houses.service.ts
+++ src/houses/houses.service.ts
@@ -13,6 +13,7 @@ import { In, Not, Repository } from 'typeorm';
 import { CreateHouseDto, UpdateHouseDto } from './houses-dto';
 import { DB_ERROR_CODES } from '../db/constants';
 import { USER_ENTITY_KEYS } from '../entities/user.entity';
+import { HOUSE_MEMBER_FOREIGN_KEYS } from '../entities/houseMember.entity';
 
 @Injectable()
 export class HousesService {
@@ -42,12 +43,21 @@ export class HousesService {
           [USER_ENTITY_KEYS.ID]:
             createDto[HOUSE_ENTITY_KEYS.OWNER][USER_ENTITY_KEYS.ID],
         },
+        ...createDto[HOUSE_ENTITY_KEYS.MEMBERS].map((member) => ({
+          [USER_ENTITY_KEYS.ID]: member,
+        })),
       ],
     });
 
     await this.housesRepository.save(house).catch((err: any) => {
       if (err.code === DB_ERROR_CODES.UNIQUE_CONSTRAINT)
         throw new ConflictException('House already exists');
+      else if (err.code === DB_ERROR_CODES.FOREIGN_KEY_VIOLATION)
+        if (err.constraint === HOUSE_MEMBER_FOREIGN_KEYS.USER)
+          throw new ConflictException('User not found');
+        else if (err.constraint === HOUSE_MEMBER_FOREIGN_KEYS.HOUSE)
+          throw new ConflictException('House not found');
+
       throw new InternalServerErrorException("Can't create house");
     });
 
@@ -66,12 +76,14 @@ export class HousesService {
         [HOUSE_ENTITY_KEYS.ID]: id,
         [HOUSE_ENTITY_KEYS.STATUS]: Not(In([HOUSE_STATUSES.DELETED])),
       },
+      relations: [HOUSE_ENTITY_KEYS.MEMBERS],
     });
 
     if (!house) throw new NotFoundException('House not found');
 
     await this.housesRepository
-      .update(id, {
+      .save({
+        ...house,
         ...updateDto,
         ...(updateDto[HOUSE_ENTITY_KEYS.LOCATION] && {
           [HOUSE_ENTITY_KEYS.LOCATION]: () =>
@@ -79,10 +91,21 @@ export class HousesService {
               updateDto[HOUSE_ENTITY_KEYS.LOCATION]
             })')`,
         }),
+        [HOUSE_ENTITY_KEYS.MEMBERS]: updateDto[HOUSE_ENTITY_KEYS.MEMBERS].map(
+          (member) => ({
+            [USER_ENTITY_KEYS.ID]: member,
+          }),
+        ),
       })
       .catch((err: any) => {
         if (err.code === DB_ERROR_CODES.UNIQUE_CONSTRAINT)
           throw new ConflictException('House already exists');
+        else if (err.code === DB_ERROR_CODES.FOREIGN_KEY_VIOLATION)
+          if (err.constraint === HOUSE_MEMBER_FOREIGN_KEYS.USER)
+            throw new ConflictException('User not found');
+          else if (err.constraint === HOUSE_MEMBER_FOREIGN_KEYS.HOUSE)
+            throw new ConflictException('House not found');
+
         throw new InternalServerErrorException("Can't update house");
       });
 
