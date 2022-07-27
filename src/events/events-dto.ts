790cbe30e62fb3d7a6ae35022654bf70f805f55b
Nick Settler <moiseevnikita14@gmail.com>
2022-07-27 22:16:49 +0200

    (feat) adds events filters (#52)

--- src/events/events-dto.ts
+++ src/events/events-dto.ts
@@ -1,6 +1,57 @@
-import { IsDateString, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
+import {
+  IsDateString,
+  IsIn,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsUUID,
+} from 'class-validator';
 import { EVENT_ENTITY_KEYS } from '../entities/event.entity';
 import { ApiProperty } from '@nestjs/swagger';
+
+export enum EVENT_FILTERS {
+  DIRECTION = 'direction',
+}
+
+export enum EVENT_DIRECTIONS {
+  UPCOMING = 'upcoming',
+  FINISHED = 'finished',
+}
+
+export class GetEventsFilterDTO {
+  @ApiProperty({
+    name: EVENT_ENTITY_KEYS.HOUSE,
+    description: "Event house's id",
+    type: 'string',
+    required: false,
+    example: '5e8f8f8f-8f8f-8f8f-8f8f-8f8f8f8f8f8f',
+  })
+  @IsOptional()
+  @IsUUID()
+  [EVENT_ENTITY_KEYS.HOUSE]: string;
+
+  @ApiProperty({
+    name: EVENT_ENTITY_KEYS.OWNER,
+    description: "Event owner's id",
+    type: 'number',
+    required: false,
+    example: 1,
+  })
+  @IsOptional()
+  @IsNumberString()
+  [EVENT_ENTITY_KEYS.OWNER]: number;
+
+  @ApiProperty({
+    name: EVENT_FILTERS.DIRECTION,
+    description: `Event direction (${EVENT_DIRECTIONS.UPCOMING} / ${EVENT_DIRECTIONS.FINISHED})`,
+    type: 'string',
+    required: false,
+    example: EVENT_DIRECTIONS.UPCOMING,
+  })
+  @IsOptional()
+  @IsIn(Object.values(EVENT_DIRECTIONS))
+  [EVENT_FILTERS.DIRECTION]: EVENT_DIRECTIONS;
+}
 
 export class CreateEventDto {
   @ApiProperty({
